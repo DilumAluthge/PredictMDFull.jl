9a017274ac9ac7ddea76f7f1c295f75f5b267f80
Dilum Aluthge <dilum@aluthge.com>
2018-08-13 00:24:26 -0400

    Fix typographical errors

--- src/init.jl
+++ src/init.jl
@@ -4,7 +4,7 @@ function __init__()::Nothing
     @info(string("This is PredictMDFull, version ", version()))
     @debug(string("PredictMDFull debugging messages are enabled"))
     @info(string("For help, please visit https://www.predictmd.net"))
-    @info(string("PredictMD package directory: \"", PredictMD.pkg_dir(), "\""))
+    @info(string("PredictMDFull package directory: \"", pkg_dir(), "\""))
     return nothing
 end
 
