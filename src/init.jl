77f32f520f78fb3df001b7638489ba70520f7064
Dilum Aluthge <dilum@aluthge.com>
2018-08-13 21:30:01 -0400

    Update init.jl

--- src/init.jl
+++ src/init.jl
@@ -1,9 +1,9 @@
 ##### Beginning of file
 
 function __init__()::Nothing
-    @info(string("This is PredictMDFull, version ", version()))
-    @debug(string("PredictMDFull package directory: \"", pkg_dir(), "\""))
-    @info(string("For help, please visit https://www.predictmd.net"))
+    println(string("This is PredictMDFull, version ", version()))
+    println(string("PredictMDFull package directory: \"", pkg_dir(), "\""))
+    println(string("For help, please visit https://www.predictmd.net"))
     return nothing
 end
 
