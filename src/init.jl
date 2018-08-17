3971958e4e173a553c0de63d6e529f5ad4d4b838
Dilum Aluthge <dilum@aluthge.com>
2018-08-16 22:17:16 -0400

    Remove commented-out lines

--- src/init.jl
+++ src/init.jl
@@ -1,9 +1,6 @@
 ##### Beginning of file
 
 function __init__()::Nothing
-    # println(string("This is PredictMDFull, version ", version()))
-    # println(string("PredictMDFull package directory: \"", pkg_dir(), "\""))
-    # println(string("For help, please visit https://www.predictmd.net"))
     print_welcome_message()
     return nothing
 end
