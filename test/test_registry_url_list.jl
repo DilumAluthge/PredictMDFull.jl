61056236f5ac670ff530b929e9bc94a60a467ec5
Dilum Aluthge <dilum@aluthge.com>
2019-02-24 00:39:19 -0500

    Update test_registry_url_list.jl

--- test/test_registry_url_list.jl
+++ test/test_registry_url_list.jl
@@ -3,7 +3,7 @@
 import PredictMDFull
 import Test
 
-Test.@test(typeof(PredictMDExtra._registry_url_list()) <: Vector{String})
-Test.@test(length(PredictMDExtra._registry_url_list()) > 0)
+Test.@test(typeof(PredictMDFull._registry_url_list()) <: Vector{String})
+Test.@test(length(PredictMDFull._registry_url_list()) > 0)
 
 ##### End of file
